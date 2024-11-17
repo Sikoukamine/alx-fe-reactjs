d22ebd4030c9c63448fafdaa3af5907808c314fc
amine <sikouk.amine@gmail.com>
2024-11-17 15:22:10 +0100

    Fix App.jsx path and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import AddRecipeForm from './AddRecipeForm';  // Correct the path if needed
-import RecipeList from './RecipeList';  // Correct the path if needed
+import React from "react";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <h1>Recipe Sharing App</h1>
+    <AddRecipeForm />
+    <RecipeList />
+  </div>
+);
 
 export default App;
