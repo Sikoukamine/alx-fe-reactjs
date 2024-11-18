019bc9200cee7c489300befce7d28564b7e20df1
Sikoukamine <sikouk.amine@gmail.com>
2024-11-18 15:37:10 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -6,7 +6,6 @@ import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetail from './components/RecipeDetail';
-import RegistrationForm from './components/RegistrationForm'; // Import the RegistrationForm component
 import FormikForm from './components/FormikForm'; // Import the FormikForm component
 
 const App = () => {
@@ -18,11 +17,10 @@ const App = () => {
         <AddRecipeForm />
         <FavoritesList />
         <RecommendationsList />
+        <FormikForm /> {/* Render the FormikForm directly */}
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipes/:id" element={<RecipeDetail />} />
-          <Route path="/register-controlled" element={<RegistrationForm />} /> {/* Route for controlled form */}
-          <Route path="/register-formik" element={<FormikForm />} /> {/* Route for Formik form */}
         </Routes>
       </div>
     </Router>
